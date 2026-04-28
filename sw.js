29fa92d1f5284fe383f1f26428202aabd7441a7e
edfilhotavares <tsf.edson@gmail.com>
2026-04-27 23:09:23 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "gastos-v26";
+const CACHE = "gastos-v27";
 const ASSETS = ["./", "index.html", "manifest.json", "icon-192.svg", "icon-512.svg"];
 
 self.addEventListener("install", e => {
